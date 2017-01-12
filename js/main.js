7434f3445e7fb19aa90b61038f73a5769069c63b
praneethy91 <praneethy91@gmail.com>
2017-01-11 22:29:14 -0500

    Adding level functionality to the cat clicker

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
+/* The various levels the cat can be in */
+var levels = ['newborn', 'infant', 'teen', 'young adult', 'mature adult', 'old', 'dead'];
+
 var ViewModel = function(){
     this.catName = ko.observable('Becky');
     this.catClicks = ko.observable(0);
+    this.catLevel = ko.observable(levels[0]);
     this.catImage = ko.observable('img/cat.jpg');
 
     this.catClicksHeader = ko.computed(function(){
         return 'The number of clicks is: ' +  this.catClicks();
     }, this);
+    this.levelHeader = ko.computed(function(){
+        return 'Thie level of cat is: ' + this.catLevel();
+    }, this);
 
     this.onCatClick = function(){
         this.catClicks(this.catClicks() + 1);
+
+        /* Circles from start to end of levels and back to first level,
+         * level increase every 10 clicks.
+         */
+        this.catLevel(levels[Math.floor(this.catClicks()/10) % levels.length]);
     }
 }
 
